d223f781cce676556233f12253a3aa2d66fc2cdd
renzon <renzon@gmail.com>
2015-09-09 16:48:57 -0300

    Savalmento com adição de linha

--- crudjquery/categoria.js
+++ crudjquery/categoria.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   var $formCategoria = $('#form-categoria');
   var $helpBlockSpam = $('span.help-block');
   var $inputNome = $('#input-nome');
+  var $tabelaCategoria = $('#tabela-categoria');
 
   $formDiv.hide();
 
@@ -26,19 +27,37 @@ $(document).ready(function () {
     });
   }
 
+  function adicionarCategoria(categoria) {
+    var linhaTabela='<tr>';
+    linhaTabela+='<td>'+ categoria.id+'</td>';
+    linhaTabela+='<td>'+ categoria.creation+'</td>';
+    linhaTabela+='<td>'+ categoria.nome+'</td>';
+    linhaTabela+= '<td>';
+
+    linhaTabela+='<button class="btn btn-danger btn-sm"><i class="glyphicon glyphicon-trash"></i></button>';
+    linhaTabela+='</td></tr>';
+    $tabelaCategoria.prepend(linhaTabela);
+    $inputNome.val('');
+  }
+
   $formCategoria.submit(function (evento) {
     evento.preventDefault();
     limparMensagensDeErro();
-    if ($inputNome.val() === '') {
+    var nome = $inputNome.val();
+    if (nome === '') {
       mostrarErros({
         'nome': 'Campo Obrigatório',
         'idade': 'idade Obrigatória'
       });
+    } else {
+      adicionarCategoria({
+        "nome": nome,
+        "id": 5910974510923776,
+        "creation": "09/08/2015 16:44:20"
+      })
     }
 
   });
-
-
 });
 
 
