13a4287745c81433c99da272ee1a3224bd8fcd68
renzon <renzon@gmail.com>
2015-09-23 17:20:27 -0300

    Criação de diretiva para linha de tabela

--- crudangular/categoria-components.js
+++ crudangular/categoria-components.js
@@ -1,35 +1,35 @@
-(function(){
-  var categoriaModulo = angular.module('categoriaModulo',['categoriaServicos']);
+(function () {
+  var categoriaModulo = angular.module('categoriaModulo', ['categoriaServicos']);
 
   categoriaModulo.directive('categoriaForm',
-  function(){
-    return {
-      restric: 'E',
-      templateUrl: 'categoria-form.html',
-      replace: true,
-      scope:{},
-      controller: function($scope, CategoriaAPI){
-        $scope.categoria={nome:'Notebooks'};
-        $scope.erros={};
-        $scope.salvandoCategoriaFlag=false;
+    function () {
+      return {
+        restric: 'E',
+        templateUrl: 'categoria-form.html',
+        replace: true,
+        scope: {},
+        controller: function ($scope, CategoriaAPI) {
+          $scope.categoria = {nome: 'Notebooks'};
+          $scope.erros = {};
+          $scope.salvandoCategoriaFlag = false;
 
-        $scope.salvar = function(){
-          $scope.erros={};
-          $scope.salvandoCategoriaFlag=true;
-          CategoriaAPI.salvar($scope.categoria, function(categoria){
-            console.log(categoria);
+          $scope.salvar = function () {
+            $scope.erros = {};
+            $scope.salvandoCategoriaFlag = true;
+            CategoriaAPI.salvar($scope.categoria, function (categoria) {
+              console.log(categoria);
 
-          },function(erros){
-            $scope.erros=erros;
-          },function(){
-            $scope.salvandoCategoriaFlag=false;
-          });
+            }, function (erros) {
+              $scope.erros = erros;
+            }, function () {
+              $scope.salvandoCategoriaFlag = false;
+            });
+
+          }
 
         }
-
-      }
-    };
-  });
+      };
+    });
 
 
   categoriaModulo.directive('categoriaTabela',
@@ -40,18 +40,36 @@
         replace: true,
         scope: {},
         controller: function ($scope, CategoriaAPI) {
-          $scope.categorias=[];
-          $scope.carregandoCategoriasFlag=true;
+          $scope.categorias = [];
+          $scope.carregandoCategoriasFlag = true;
 
-          CategoriaAPI.listar(function(categorias){
-            $.each(categorias,function(index, cat){
+          CategoriaAPI.listar(function (categorias) {
+            $.each(categorias, function (index, cat) {
               $scope.categorias.push(cat);
             });
-          }, function(){
+          }, function () {
 
-          },function(){
-            $scope.carregandoCategoriasFlag=false;
+          }, function () {
+            $scope.carregandoCategoriasFlag = false;
           });
+
+
+        }
+      };
+    });
+
+
+  categoriaModulo.directive('categoriaLinha',
+    function () {
+      return {
+        restric: 'A',
+        templateUrl: 'categoria-linha.html',
+        replace: true,
+        scope: {
+          categoria:'='
+        },
+        controller: function ($scope, CategoriaAPI) {
+
 
 
         }
